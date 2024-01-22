bae5cf324a895a5e34d9cd40c0c5ae42eda03974
kopatich32 <kopatich1994@yandex.ru>
2024-01-22 15:36:57 +0300

    .

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="player">
+
+    <aside class="aside-left">
+        <nav class="links">
+            <ul>
+                <li><span><img src="icons/house-chimney.svg" alt="main-page"></span>Home</li>
+                <li><span><img src="icons/flame.svg" alt="tranding"></span>Trending</li>
+                <li><span><img src="icons/following.svg" alt="following"></span>Following</li>
+            </ul>
+        </nav>
+        <nav class="playlists">
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+            <div>5</div>
+            <div>6</div>
+            <div>7</div>
+            <div>8</div>
+        </nav>
+    </aside>
+    <div class="cnt">
+        <header>
+            <label>
+                <img src="icons/search.svg" alt="search">
+                <input type="text" placeholder="i want to listen...">
+            </label>
+            <div class="notification"><img src="icons/bell.svg" alt="notification"></div>
+            <div class="profile"><img src="icons/vse-ochen-ploho.jpg" alt="user-photo "></div>
+        </header>
+        <main>
+            <div class="podcast-img"><img src="icons/podcast.png" alt="podcast-picture"></div>
+        </main>
+    </div>
+</div>
+<footer></footer>
+
+</body>
+</html>
