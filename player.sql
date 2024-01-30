ae758e6f44a5a6a4b64e81f3954f1bc681b34cb8
kopatich32 <kopatich1994@yandex.ru>
2024-01-30 19:22:47 +0300

    .

---
+++ player.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 30 2024 г., 19:22
+-- Версия сервера: 10.4.26-MariaDB
+-- Версия PHP: 8.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `player`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `tracks`
+--
+
+CREATE TABLE `tracks` (
+  `id` int(255) NOT NULL,
+  `cover` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `track_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `artist` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `listening` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `tracks`
+--
+
+INSERT INTO `tracks` (`id`, `cover`, `track_name`, `artist`, `duration`, `listening`, `link`) VALUES
+(1, 'covers/Infinite_Granite_2021.png', 'The Gnashing', 'Deafheaven', '5:34', '123', 'tracks/Deafheaven_-_The_Gnashing.mp3'),
+(2, 'covers/Madmans_Esprit.jpg', '내버려둬 Leave Me Alone', 'Madmans Esprit', '4:14', '127', 'tracks/Madmans_Esprit_-_내버려둬_Leave_Me_Alone.mp3'),
+(3, 'covers/The_Missing_Man.jpg', 'Trash Bat', 'AFI', '2:07', '257', 'tracks/AFI_-_Trash_Bat.mp3');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `tracks`
+--
+ALTER TABLE `tracks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `tracks`
+--
+ALTER TABLE `tracks`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
