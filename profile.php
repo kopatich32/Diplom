eb6c4d9f101e309860d4b52fa0eceb1b6983a8a9
kopatich32 <kopatich1994@yandex.ru>
2024-03-19 21:08:40 +0300

    добавление в базу

--- profile.php
+++ profile.php
@@ -11,18 +11,30 @@
 <?php
 include 'get_tracks.php';
 	include('getID3-master/getid3/getid3.php');
-	global $connect;
-	$id = new getID3();
-	$tag = 'utf-8';
-	$id->setOption(['encoding'=>$tag]);
-	include "getID3-master/getid3/write.php";
-	$fileName = $_FILES['FILE']['tmp_name'];
-	$fileinfo = $id->analyze($fileName);
-	$img = $fileinfo['id3v2']['APIC'][0]['data'];
-	echo '<pre>' . print_r($img,true) . '</pre>';
+include "getID3-master/getid3/write.php";
+global $connect;
+if(isset($_FILES['FILE']['size']) && $_FILES['FILE']['size'] > 0){
+    $id = new getID3();
+    @$fileName = $_FILES['FILE']['tmp_name'];
+    @$fileInfo = $id->analyze($fileName);
+    if($fileInfo['id3v2']['APIC'][0]['data']){
+        $img = $fileInfo['id3v2']['APIC'][0]['data'];
+        $addedTimeCover = time();
+        @$file = file_put_contents(__DIR__."/covers/".$addedTimeCover.".png", $img);
+        $getArtist = explode('-',$_FILES['FILE']['name'])[0];
+        $getTrackName = explode('-',$_FILES['FILE']['name'])[1];
+    }
+    $getDuration =  $fileInfo['playtime_string'];
+    $linkNewtrack = 'tracks/'.$_FILES['FILE']['full_path'];
+    $coverName = 'covers/'.$addedTimeCover;
+    $query = $connect->query( "INSERT INTO `tracks`(`cover`, `track_name`, `artist`, `duration`,`listening`,`link`) VALUES ('$coverName','$getTrackName','$getArtist','$getDuration','12','$linkNewtrack')");
+
+}
+
+
 
 ?>
-<img src="<?=$img?>" alt="">
+<img src="<?=realpath($file)?>" alt="">
 <form action="" method="POST" enctype="multipart/form-data">
 	<input name="FILE" type="file" placeholder="mp3 track only">
 	<button name="SUBMIT" type="submit">send</button>
